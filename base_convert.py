c9c2b86d2a5dc2d6877f64987b699e4311457ffd
jyang104calpoly <jyang104@calpoly.edu>
2021-01-17 23:32:48 -0800

    a

--- base_convert.py
+++ base_convert.py
@@ -4,22 +4,11 @@
 # converts a base 10 number to base b by returning a string representation of it, using recursion
 def convert(num, b):
     """Recursive function that returns a string representing num in the base b"""
-    if num//b == 0: 
-        a = str(num%b)
-        if a == "10":
-            return "A"
-        elif a == "11":
-            return "B"
-        elif a == "12":  
-            return "C"
-        elif a == "13":  # converts a number greater than 10 at the very front into alphabet                    
-            return "D"   # ex) "10BCDEF" -> "ABCDEF"
-        elif a == "14":
-            return "E"
-        elif a == "15":
-            return "F"
+    if num//b == 0 and num < 10:
+        if num == 0:
+            return ""
         else:
-            return a
+            return str(num%b) # base case
     else:
         """solve for remainder (num%b) and quotient (num//b) and return the string representation of the remainder by using recursion"""
         if num%b == 10: # a case where a remainder is 10
